d5492b396aea2036b823c8ad60612e767f6611b7
Saketh Surya <raisingsurya05@gmail.com>
2025-01-11 13:57:35 +0530

    Added database schema
    Used MongoDB Atlas connection string and created new database and collection to store cryptos data.

---
+++ models/CryptoData.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const cryptoDataSchema = new mongoose.Schema({
+  coinId: { type: String, required: true },
+  price: { type: Number, required: true },
+  marketCap: { type: Number, required: true },
+  change24h: { type: Number, required: true },
+  timestamp: { type: Date, default: Date.now },
+});
+
+// Update the connection string with your MongoDB Atlas connection string
+const uri = process.env.MONGO_DB_ATLAS_URI;
+mongoose
+  .connect(uri,{dbName:"CoinGeckoData"})
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB Atlas:", error);
+  });
+
+module.exports = mongoose.model("CryptoData", cryptoDataSchema, "cryptoData");
