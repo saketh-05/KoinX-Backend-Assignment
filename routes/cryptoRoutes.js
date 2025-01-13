77c35f1a705f89d9a56c2eb1d1769f27f1782535
Saketh Surya <raisingsurya05@gmail.com>
2025-01-13 17:00:37 +0530

    Added error handling
    Added error handling/handling unexpected behaviour for invalid parameters in query for /stats and /deviation.

--- routes/cryptoRoutes.js
+++ routes/cryptoRoutes.js
@@ -7,19 +7,22 @@ require("dotenv").config();
 const fetchData = async () => {
   try {
     //console.log("Fetching data from the API");
-    const response = await axios.get("https://api.coingecko.com/api/v3/simple/price",{
-      params: {
-        ids: "bitcoin,ethereum,matic-network",
-        vs_currencies: "usd",
-        include_market_cap: "true",
-        include_24hr_change: "true",
-      },
-      headers: {
-        "Content-Type": "application/json",
-        "x-cg-demo-api-key": process.env.COIN_GECKO_API,
-      },
-      method: "GET",
-    });
+    const response = await axios.get(
+      "https://api.coingecko.com/api/v3/simple/price",
+      {
+        params: {
+          ids: "bitcoin,ethereum,matic-network",
+          vs_currencies: "usd",
+          include_market_cap: "true",
+          include_24hr_change: "true",
+        },
+        headers: {
+          "Content-Type": "application/json",
+          "x-cg-demo-api-key": process.env.COIN_GECKO_API,
+        },
+        method: "GET",
+      }
+    );
     //console.log("Data fetched from the API successfully - ", response.data);
     const istoffset = 5.5 * 60;
     const data = Object.keys(response.data).map((coinId) => {
@@ -28,9 +31,9 @@ const fetchData = async () => {
       return {
         coinId,
         price: coin.usd,
-        marketCap: coin. usd_market_cap,
+        marketCap: coin.usd_market_cap,
         change24h: coin.usd_24h_change,
-        timestamp: new Date(date.getTime() + (istoffset) * 60000),
+        timestamp: new Date(date.getTime() + istoffset * 60000),
       };
     });
     //console.log("Saving data to the database, data:",data);
@@ -44,6 +47,20 @@ const fetchData = async () => {
 
 router.get("/stats", async (req, res) => {
   const { coin } = req.query;
+  if (!coin) {
+    return res.status(400).json({ error: "Coin parameter is required" });
+  } else if (
+    coin !== "bitcoin" &&
+    coin !== "ethereum" &&
+    coin !== "matic-network"
+  ) {
+    return res
+      .status(400)
+      .json({
+        error:
+          "Invalid coin parameter, Permitted coins = bitcoin, ethereum and matic-network",
+      });
+  }
   try {
     const data = await CryptoData.findOne({ coinId: coin }).sort({
       timestamp: -1,
@@ -66,7 +83,18 @@ router.get("/stats", async (req, res) => {
 
 router.get("/deviation", async (req, res) => {
   const { coin } = req.query;
-
+  if (!coin) {
+    return res.status(400).json({ error: "Coin parameter is required" });
+  } else if (
+    coin !== "bitcoin" &&
+    coin !== "ethereum" &&
+    coin !== "matic-network"
+  ) {
+    return res.status(400).json({
+      error:
+        "Invalid coin parameter, Permitted coins = bitcoin, ethereum and matic-network",
+    });
+  }
   try {
     const records = await CryptoData.find({ coinId: coin })
       .sort({ timestamp: -1 })
